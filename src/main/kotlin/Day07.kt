e99df3d2715f99fba1d535264007e03c2ffba9a9
Joost Baas <joost@joostbaas.eu>
2023-12-07 22:37:26 +0200

    use comparators, is actually slower and fails part2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,10 +1,79 @@
-
 fun String.isFiveOfAKind(): Boolean = cardCounts().maxOf { it.value } == 5
 fun String.isFourOfAKind(): Boolean = cardCounts().maxOf { it.value } == 4
 fun String.isFullHouse(): Boolean = cardCounts().values.sorted() == listOf(2, 3)
 fun String.isThreeOfAKind(): Boolean = cardCounts().maxOf { it.value } == 3
 fun String.isTwoPair(): Boolean = cardCounts().values.sorted() == listOf(1, 2, 2)
 fun String.isPair(): Boolean = cardCounts().maxOf { it.value } == 2
+
+private class HighCardComparator(val cards: List<Char>) : Comparator<String> {
+    override fun compare(o1: String, o2: String): Int =
+        when {
+            o1.isEmpty() && o2.isEmpty() -> 0
+            o1.isEmpty() && o2.isNotEmpty() -> -1
+            o1.isNotEmpty() && o2.isEmpty() -> 1
+            else -> {
+                val firstCharCompared = cards.indexOf(o2[0]).compareTo(cards.indexOf(o1[0]))
+                if (firstCharCompared == 0) {
+                    this.compare(o1.drop(1), o2.drop(1))
+                } else firstCharCompared
+            }
+        }
+}
+
+private object HandSetComparator : Comparator<String> {
+
+    private fun comparator(fromBool: (String) -> Boolean): Comparator<String> =
+        Comparator { o1, o2 ->
+            val fromBool1 = fromBool(o1)
+            val fromBool2 = fromBool(o2)
+            when {
+                fromBool1 == fromBool2 -> 0
+                fromBool1 -> 1
+                else -> -1
+            }
+        }
+
+    override fun compare(hand1: String, hand2: String): Int =
+        comparator(String::isFiveOfAKind)
+            .thenComparing(comparator(String::isFourOfAKind))
+            .thenComparing(comparator(String::isFullHouse))
+            .thenComparing(comparator(String::isThreeOfAKind))
+            .thenComparing(comparator(String::isTwoPair))
+            .thenComparing(comparator(String::isPair))
+            .compare(hand1, hand2)
+}
+
+val cards = listOf( //A22 -> 1200 vs KQ5 -> 1100+100+5 -> 1205
+    'A', 'K', 'Q', 'J', 'T',
+    '9', '8',
+    '7', '6', '5', '4', '3', '2'
+)
+
+private val NormalHandComparator = HandSetComparator.thenComparing(HighCardComparator(cards))
+
+private object HandWithJokerComparator : Comparator<String> {
+
+    private fun String.generateAlternatives(): List<String> =
+        when {
+            this.isEmpty() -> emptyList()
+            this[0] == 'J' -> cardsWithoutJoker
+                .flatMap { newChar ->
+                    (newChar + this.drop(1)).generateAlternatives()
+                }
+            else -> listOf(this) + this.drop(1).generateAlternatives().map {
+                this[0] + it
+            }
+        }
+
+    override fun compare(hand1: String, hand2: String): Int {
+        val hand1Alternatives: List<String> = hand1.generateAlternatives()
+        val hand2Alternatives: List<String> = hand2.generateAlternatives()
+        val bestHand1Alternative = hand1Alternatives.sortedWith(HandSetComparator).last()
+        val bestHand2Alternative = hand2Alternatives.sortedWith(HandSetComparator).last()
+        return HandSetComparator.thenComparing(HighCardComparator(cardsWithJokerLast))
+            .compare(bestHand1Alternative, bestHand2Alternative)
+    }
+}
 
 private fun String.cardCounts() =
     this.toSet()
@@ -20,6 +89,7 @@ fun String.handScoreWithJokers(): Long {
                 .flatMap { newChar ->
                     (newChar + this.drop(1)).generateAlternatives()
                 }
+
             else -> listOf(this) + this.drop(1).generateAlternatives().map {
                 this[0] + it
             }
@@ -39,12 +109,9 @@ fun String.handSetScore(): Long =
     }
 
 fun String.handScore(cardsInOrder: List<Char>): Long =
-     handSetScore() + highCard(cardsInOrder)
+    handSetScore() + highCard(cardsInOrder)
 
-val cards = listOf( //A22 -> 1200 vs KQ5 -> 1100+100+5 -> 1205
-    'A', 'K', 'Q', 'J', 'T',
-    '9', '8',
-    '7', '6', '5', '4', '3', '2')
+
 
 val cardsWithoutJoker = cards.filterNot { it == 'J' }
 val cardsWithJokerLast = cardsWithoutJoker + 'J'
@@ -64,12 +131,13 @@ fun day07Part1(input: List<String>): Long {
     val sortedHands = input.map {
         it.split(" ")
     }.map { (hand, bid) ->
-        hand.handScore(cards) to bid.toInt()
-    }.sortedBy {
-        it.first
+        hand to bid.toInt()
+    }.sortedWith { o1, o2 ->
+        NormalHandComparator.compare(o1.first, o2.first)
     }
+
     return sortedHands.foldIndexed(0) { index, acc, (_, bid) ->
-        acc + ((index+1) * bid)
+        acc + ((index + 1) * bid)
     }
 }
 
@@ -77,11 +145,11 @@ fun day07Part2(input: List<String>): Int {
     val sortedHands = input.map {
         it.split(" ")
     }.map { (hand, bid) ->
-        hand.handScoreWithJokers() to bid.toInt()
-    }.sortedBy {
-        it.first
+        hand to bid.toInt()
+    }.sortedWith { o1, o2 ->
+        HandWithJokerComparator.compare(o1.first, o2.first)
     }
     return sortedHands.foldIndexed(0) { index, acc, (_, bid) ->
-        acc + ((index+1) * bid)
+        acc + ((index + 1) * bid)
     }
 }
--- src/test/kotlin/Day07.kt
+++ src/test/kotlin/Day07.kt
@@ -1,9 +1,5 @@
-import io.kotest.matchers.longs.shouldBeGreaterThan
-import io.kotest.matchers.longs.shouldBeLessThan
 import io.kotest.matchers.shouldBe
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.DynamicTest.dynamicTest
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 
 class Day07 {
@@ -16,23 +12,6 @@ class Day07 {
         QQQJA 483
     """.trimIndent().lines()
     private val input = readInput("Day2023_07")
-
-    @Test
-    fun scores() {
-        assertTrue("32T3K".isPair())
-        assertTrue("T55J5".isThreeOfAKind())
-        assertTrue("KK677".isTwoPair())
-        assertTrue("KTJJT".isTwoPair())
-        assertTrue("QQQJA".isThreeOfAKind())
-
-        "32222".highCard(cards) shouldBeGreaterThan "22222".highCard(cards)
-        "23222".highCard(cards) shouldBeGreaterThan "22322".highCard(cards)
-        "KK222".highCard(cards) shouldBeGreaterThan "KT222".highCard(cards)
-        "2222A".highCard(cards) shouldBeGreaterThan "2222K".highCard(cards)
-        "KTJJ2".handScore(cards) shouldBeLessThan "KK672".handScore(cards)
-
-        "JKKK2".handScoreWithJokers() shouldBeLessThan "QQQQ2".handScoreWithJokers()
-    }
 
     @TestFactory
     fun `part 1`() =
